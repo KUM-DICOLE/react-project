06f0641c2ac9b734ab8f45be7e2c0e1f3e6197ba
Kum Dicole <kumdicole@gmail.com>
2024-09-06 17:13:28 -0700

    Login and useState Hook, Events

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,7 +17,7 @@ type ButtonProps = {
      }: ButtonProps){
         return (
         <button 
-        className={`py-2 px-9 flex gap-4 justify-center rounded-full text-white border-2 border-white 
+        className={`py-2 px-9 flex gap-3 justify-center items-center rounded-full text-white border-2 border-white 
             hover:bg-myPink transition-all hover:drop-shadow-lg ${
                 secondary ? "bg-myPink" : "bg-myBlue"}
         ${className} ${loading && 'cursor-wait'}`}
